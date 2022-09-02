7bfcbee5dae636f2ead7712ab21b0dc6ca87553a
human <95416949+human-school@users.noreply.github.com>
2022-09-02 16:00:49 +1000

    SSL cert set up and download finished

---
+++ download_page.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/main.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<h3></h3>
+<meta charset="utf-8">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>download</title>
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+</head>
+<body bgcolor = "black">
+<table width="100%" border="0">
+  <tbody>
+    <tr>
+      <td width="134" height="111" valign="top"><a href="index.php"><img src="images/game-codeorg-logo-technology-computer-science-removebg-preview.png" alt="home" width="144" height="121" title="home"/></a></td>
+      <td width="1410">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="1548" valign="top"><p><a href="index.php"><img src="images/home.png" alt="home" width="150" height="80
+	 " title="home" border="0"/></a></p>
+      <p><a href="download_page.php"><img src="images/download.png" width="150" height="80" alt="download" title="download"/></a></p></td>
+      <td valign="top" style="color: #FFFFFF"><!-- InstanceBeginEditable name="EditRegion1" --><p><a href="download.php?path=Main_game.Kodu2" title="main game download">Download main game file</a></p>
+		  <p><a href="download.php?path=Tutorial.Kodu2" title="tutorial file">Download Tutorial</a></p><!-- InstanceEndEditable --></td>
+    </tr>
+  </tbody>
+</table>
+
+</body>
+<!-- InstanceEnd --></html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/main.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<h3></h3>
+<meta charset="utf-8">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>kodu is bad don't use it</title>
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+</head>
+<body bgcolor = "black">
+<table width="100%" border="0">
+  <tbody>
+    <tr>
+      <td width="134" height="111" valign="top"><a href="index.php"><img src="images/game-codeorg-logo-technology-computer-science-removebg-preview.png" alt="home" width="144" height="121" title="home"/></a></td>
+      <td width="1410">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="1548" valign="top"><p><a href="index.php"><img src="images/home.png" alt="home" width="150" height="80
+	 " title="home" border="0"/></a></p>
+      <p><a href="download_page.php"><img src="images/download.png" width="150" height="80" alt="download" title="download"/></a></p></td>
+      <td valign="top" style="color: #FFFFFF"><!-- InstanceBeginEditable name="EditRegion1" --><p>welcome to kodu users website, this is where you can download the game files&nbsp;
+		  <p>click the download button on the side bar to get the files</p></p>
+      <!-- InstanceEndEditable --></td>
+    </tr>
+  </tbody>
+</table>
+
+</body>
+<!-- InstanceEnd --></html>
